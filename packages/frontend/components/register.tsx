7160a6bcf17280b120378e741e0c8155e3e84f9c
oussama.bensalah <oussama.bensalah@amaris.com>
2024-03-07 15:11:44 +0100

    change auth buttons styles

--- packages/frontend/components/register.tsx
+++ packages/frontend/components/register.tsx
@@ -23,7 +23,7 @@ export default function Register() {
               name="password"
               className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
-            <button type="submit">Register</button>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">Register</button>
           </form>
           <span id="register-response" className="mt-5"></span>
         </div>
